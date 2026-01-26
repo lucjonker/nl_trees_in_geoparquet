ec1625d429d8678c895aad50f6a6fba7cc65b594
lucjonker <lucjonkerwork@gmail.com>
2026-01-26 17:38:00 +0100

    Try to clean s3 bucket

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
-
+import boto3
 
 def unzip_dir(directory_path):
     for filename in os.listdir(directory_path):
@@ -12,3 +12,20 @@ def unzip_dir(directory_path):
 
             with zipfile.ZipFile(full_path, 'r') as zip_ref:
                 zip_ref.extractall(extract_to)
+
+def delete_s3_item(bucket_name, object_name):
+    s3 = boto3.client('s3')
+    result = s3.list_objects(Bucket=bucket_name, Prefix="roorda-tudelft/public-trees-in-nl/", Delimiter='/')
+    for o in result.get('CommonPrefixes'):
+        if o.get('Prefix') == object_name:
+            print(f"Deleting {o.get('Prefix')}")
+            response = s3.delete_object(Bucket=bucket_name, Key=object_name)
+            print(response)
+
+
+def main():
+    # delete_s3_item("us-west-2.opendata.source.coop", "roorda-tudelft/public-trees-in-nl/geoparquet/")
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
